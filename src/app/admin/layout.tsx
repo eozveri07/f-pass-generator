59301cac94ca06b31e2e64236c878007ba8e496a
eozveri07 <eozveri07@gmail.com>
2025-01-13 06:59:18 +0300

    feat: Implement two-factor authentication setup and verification endpoints

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,17 +1,14 @@
-import { auth } from "@/auth"
+import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-
-
 export default async function AdminLayout({
-    
   children,
 }: {
   children: React.ReactNode;
 }) {
-      const session = await auth()
+  const session = await auth();
 
-  if (!session?.user) return redirect("/login")
+  if (!session?.user) return redirect("/login");
 
   return children;
 }
