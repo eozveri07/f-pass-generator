3153882ce7c0f82d2f72b0b4d840015659f6ef98
eozveri07 <eozveri07@gmail.com>
2025-01-13 03:12:42 +0300

    feat: Add admin layout component with session validation and redirection

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/auth"
+import { redirect } from "next/navigation";
+
+
+
+export default async function AdminLayout({
+    
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+      const session = await auth()
+
+  if (!session?.user) return redirect("/login")
+
+  return children;
+}
