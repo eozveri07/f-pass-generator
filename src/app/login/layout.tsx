904410d7e67472fbc24a01523d0529d09354202a
eozveri07 <eozveri07@gmail.com>
2025-01-13 03:12:37 +0300

    feat: Add login layout component with session redirection

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,14 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+
+  if (session?.user) return redirect("/admin");
+
+  return children;
+}
