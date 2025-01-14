4942534b2143338c1eac8bd427f37d8da9c25559
eozveri07 <eozveri07@gmail.com>
2025-01-15 00:55:19 +0300

    feat: Enhance logout functionality by removing master key cookie before logout action

--- src/app/admin/logout.tsx
+++ src/app/admin/logout.tsx
@@ -1,14 +1,20 @@
 "use client";
-
 import { LogOut } from "lucide-react";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { handleLogout } from "./action";
+import Cookies from "js-cookie";
 
 export default function LogoutButton() {
+  const handleClick = async () => {
+    Cookies.remove("master_key");
+
+    await handleLogout();
+  };
+
   return (
     <DropdownMenuItem
       className="text-red-600 cursor-pointer"
-      onClick={() => handleLogout()}
+      onClick={handleClick}
     >
       <LogOut className="h-4 w-4 mr-2" />
       Çıkış Yap
