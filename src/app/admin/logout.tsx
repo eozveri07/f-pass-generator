22fffa218b30351025260446210d05485ad9218b
eozveri07 <eozveri07@gmail.com>
2025-01-26 03:41:56 +0300

    feat: refactor logout button to include user account navigation

--- src/app/admin/logout.tsx
+++ src/app/admin/logout.tsx
@@ -1,23 +1,32 @@
 "use client";
-import { LogOut } from "lucide-react";
+import { LogOut, User } from "lucide-react";
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { handleLogout } from "./action";
+import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 
-export default function LogoutButton() {
+export default function UserDropdownButtons() {
+  const router = useRouter();
+  
   const handleClick = async () => {
     Cookies.remove("master_key");
-
     await handleLogout();
   };
 
   return (
-    <DropdownMenuItem
-      className="text-red-600 cursor-pointer"
-      onClick={handleClick}
-    >
-      <LogOut className="h-4 w-4 mr-2" />
-      Çıkış Yap
-    </DropdownMenuItem>
+    <>
+      <DropdownMenuItem onClick={() => router.push("/admin/account")}>
+        <User className="h-4 w-4 mr-2" />
+        Hesabım
+      </DropdownMenuItem>
+      
+      <DropdownMenuItem
+        className="text-red-600 cursor-pointer"
+        onClick={handleClick}
+      >
+        <LogOut className="h-4 w-4 mr-2" />
+        Çıkış Yap
+      </DropdownMenuItem>
+    </>
   );
 }
